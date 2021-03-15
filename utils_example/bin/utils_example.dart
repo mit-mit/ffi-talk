b7354ef0ed5f8de9791aa95fa86df2f51f1bdda4
Daco Harkes <dacoharkes@google.com>
2021-03-15 13:30:56 +0100

    tweaks + disable hover

--- utils_example/bin/utils_example.dart
+++ utils_example/bin/utils_example.dart
@@ -1,15 +1,20 @@
+// Copyright (c) 2021, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'dart:ffi';
+
 import 'package:ffi/ffi.dart';
 
 void main(List<String> arguments) {
-  final utilsLib = DynamicLibrary.open('utilslib/lib/libutils.1.0.0.dylib');
-  final add = utilsLib.lookupFunction<Int32 Function(Int32, Int32),
+  final dylib = DynamicLibrary.open('utilslib/lib/libutils.1.0.0.dylib');
+  final add = dylib.lookupFunction<Int32 Function(Int32, Int32),
       int Function(int, int)>('add');
-  final result = add(2, 3);
+  final result = add(40, 2);
   print(result);
 
   final hello = 'Hello'.toNativeUtf8();
-  final reverse = utilsLib.lookupFunction<Void Function(Pointer<Utf8>),
+  final reverse = dylib.lookupFunction<Void Function(Pointer<Utf8>),
       void Function(Pointer<Utf8>)>('reverse');
   reverse(hello);
   print(hello.toDartString());
